7e4afc261a2dbcdb33ee2d52f2c113d881387d35
Leonid <laromanov@edu.hse.ru>
2023-04-15 01:53:50 +0300

    Update 14.04.2023

---
+++ Save.js
@@ -0,0 +1,36 @@
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "styles/light.css";
+    let darkTheme = "styles/dark.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+    console.log(currTheme);
+
+    if(currTheme == lightTheme)
+    {
+        currTheme = darkTheme;
+        theme = "dark";
+    }
+    else
+    {
+        currTheme = lightTheme;
+        theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
+
+function Save(theme)
+{
+    var Request = new XMLHttpRequest();
+    Request.open("GET", "./themes.php?theme=" + theme, true); //У вас путь может отличаться
+    Request.send();
+}
